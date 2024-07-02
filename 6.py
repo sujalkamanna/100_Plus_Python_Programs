b6f6e65e63c60d159995a45078b98d1533d6864b
Sujal Kamanna <sujalkamanna2003@gmail.com>
2024-07-02 18:53:33 +0530

    added 6.py

--- 6.py
+++ 6.py
@@ -1,2 +1,8 @@
-# This is 6.py
-print("Hello, World!")
+# Program 6
+# Write a Python program to convert kilometers to miles
+
+print("Python program to convert KM into miles")
+KM = float(input("Enter Km value:"))
+miles = KM*0.621371
+
+print(f"{KM} into miles is:",miles)
