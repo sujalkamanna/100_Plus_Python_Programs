88e41071df37d4cafdb70ac01186ce24749c86bb
Sujal Kamanna <sujalkamanna2003@gmail.com>
2024-07-06 23:57:18 +0530

    added 40 41

--- 40.py
+++ 40.py
@@ -5,8 +5,7 @@ str = (input("Enter words:"))
 
 lower_case = str.split()
 print(lower_case)
-
+lower_case.sort()
 print("the sorted words are:")
 for i in lower_case:
-    lower_case.sort()
     print(i)
