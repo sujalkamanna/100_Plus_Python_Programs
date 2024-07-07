c1d4a8a7fd4df0808b3458be0f7d5cafa4481d47
Sujal Kamanna <sujalkamanna2003@gmail.com>
2024-07-08 01:14:38 +0530

    "Modified code to sort list in descending order and print specified number of largest elements"

--- 54.py
+++ 54.py
@@ -5,5 +5,8 @@ list1 = [465,465,4,6,5,56,8654,89,546,3]
 print("the length of list is :",len(list1))
 
 largest = int(input("enter no of largest elements you want:"))
+sorted = list1.sort(reverse=True)
 
-print("sorted elements is ",sorted(list1[-largest]))
+list1.sort(reverse=True)
+largest_elements = list1[:largest]
+print(f"sorted elements is {largest_elements}")
