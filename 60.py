58218707fe51fc6dc94e01a325931e7e701778ca
Sujal Kamanna <sujalkamanna2003@gmail.com>
2024-07-08 21:10:42 +0530

    "Added list of words, variable k, and a for loop to print words with length greater than k."

--- 60.py
+++ 60.py
@@ -1,5 +1,10 @@
 # Program 60
 # Write a Python program to find words which are greater than given length k
 
-list1 = ["apple", "banana","pineapple","grapes","figs","friuts"]
+list1 = ["apple", "banana", "pineapple", "grapes", "figs", "friuts",]
 
+k = 5
+
+for i in list1:
+    if len(i) > k:
+        print(i)
