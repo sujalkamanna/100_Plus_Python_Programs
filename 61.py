3304330096d999599b52cc1cc035d78887e693c4
Sujal Kamanna <sujalkamanna2003@gmail.com>
2024-07-09 00:28:57 +0530

    "Added input to remove character by index, replaced hardcoded '𝑖' with dynamic character removal"

--- 61.py
+++ 61.py
@@ -1,6 +1,21 @@
-#Program 61
-# Write a Python program for removing 𝑖 character from a string. 
+# Program 61
+# Write a Python program for removing 𝑖 character from a string.
 
 string = input("enter the string:")
-string = string.replace("𝑖","")
+length = len(string)
+
+print("the string is:")
+for index, char in enumerate(string):
+    print(f"[{index}]: {char}")
+
+
+remove = 0
+
+def rem():
+    global remove
+    remove = int(input("enter char no you want to remove:"))
+
+
+rem()
+string = string.replace(string[remove], "")
 print(string)
