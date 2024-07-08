4c273e50c7d05ef41210bb8fdfbc450cc79a95d2
Sujal Kamanna <sujalkamanna2003@gmail.com>
2024-07-08 20:59:10 +0530

    Python program to find words which are greater than given length k

--- 60.py
+++ 60.py
@@ -1,2 +1,5 @@
-# This is 60.py
-print("Hello, World!")
+# Program 60
+# Write a Python program to find words which are greater than given length k
+
+list1 = ["apple", "banana","pineapple","grapes","figs","friuts"]
+
